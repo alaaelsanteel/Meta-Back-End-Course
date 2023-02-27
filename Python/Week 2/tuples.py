bcdef77e3e1ff03de8fc46817e2dacd7d92af955
Alaa Elsanteel <78312680+alaaelsanteel@users.noreply.github.com>
2023-02-27 02:15:11 +0200

    Delete Python directory

--- Python/Week 2/tuples.py
+++
@@ -1,8 +0,0 @@
-my_tuble= (1,'hello',55.4,True)
-print(my_tuble[1]) #hello
-print(type(my_tuble)) #class tuple
-print(my_tuble.count('hello'))  #1
-print(my_tuble.index(55.4)) #2
-
-#Tubles are immutable >values cann't be changed
-my_tuble[0]=2  #typeError
--- Python/Week 4/fileChanges.py
+++
@@ -1,5 +0,0 @@
-import os
-def printChange():
-   content=os.listdir('E:\Back-End\Python\Week 4')
-   print("Current content: ")
-   print(content)
